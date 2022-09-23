20630bacc53eda604001a1f7d38bdd9ab139fde1
Yuan <yuanzf@mit.edu>
2022-09-23 14:04:11 +0800

    change main color scheme

--- javascript/maphelpers.js
+++ javascript/maphelpers.js
@@ -79,19 +79,18 @@ function colorbar(num){
 
 function vibcolor(timevisit){
     if (timevisit<1097*3600){
-        text = "Low"
-        return '<h4 style = "color:#fdae61"><strong>'+text+'</strong></h4>'
+
+        return '<h4 ><strong>Low</strong></h4>'
     }else{
-        text = "High"
-        return '<h4 style = "color:#2b83ba"><strong>'+text+'</strong></h4>'
+
+        return '<h4><strong>High</strong></h4>'
     }
 }
 
+
+
 function bar4group(g1, g2, g3, g4){
-    // h1= g1/3600000
-    // h2= g2/3600000
-    // h3= g3/3600000
-    // h4= g4/3600000
+
     const hmax = 58
     h1= g1*hmax
     h2= g2*hmax
@@ -122,10 +121,10 @@ return '<svg width="200" height="76">\
 		</g>\
         <path class="domain" d="M0,6V0H900V6"></path>\
 	</g>\
-		<rect class="bar2" id ="rcorners1" x="3" width="30" y="'+height1 +'" height="'+h1.toFixed(2)+'"style="fill:#BDBEC0;"></rect>\
-		<rect class="bar2" id ="rcorners1" x="49.5" width="30"  y="'+height2 +'" height="'+h2.toFixed(2)+'"style="fill:#BDBEC0;"></rect>\
-		<rect class="bar2" id ="rcorners1" x="98" width="30"  y="'+height3 +'" height="'+h3.toFixed(2)+'"style="fill:#BDBEC0;"></rect>\
-		<rect class="bar2" id ="rcorners1" x="145.5" width="30"  y="'+height4+'" height="'+h4.toFixed(2)+'"style="fill:#BDBEC0;"></rect>\
+		<rect class="bar2" id ="rcorners1" x="3" width="30" y="'+height1 +'" height="'+h1.toFixed(2)+'"style="fill:#024bd4;"></rect>\
+		<rect class="bar2" id ="rcorners1" x="49.5" width="30"  y="'+height2 +'" height="'+h2.toFixed(2)+'"style="fill:#024bd4;"></rect>\
+		<rect class="bar2" id ="rcorners1" x="98" width="30"  y="'+height3 +'" height="'+h3.toFixed(2)+'"style="fill:#024bd4;"></rect>\
+		<rect class="bar2" id ="rcorners1" x="145.5" width="30"  y="'+height4+'" height="'+h4.toFixed(2)+'"style="fill:#024bd4;"></rect>\
 	</g>\
 </svg>'
 }
